3e57f7e6a9ab133a71e2e561468dd86a2a6644ab
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2021-04-23 20:48:56 +0300

    chore: Remove line

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
 export default useIpcListener;
-
 /**
  * Custom React Hook that listen to channel. When a new message arrives `listener` would be called with `listener(event, args...)`
  * @param {string} channel - The name of the channel
